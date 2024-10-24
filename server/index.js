80a22732cdd91a325ded8fc403fb78bec9f9cc93
RealSebasTheFish <sebastianq04@gmail.com>
2024-10-24 18:26:38 -0400

    Server and basic client commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const socket = require("socket.io");
+
+// App setup
+const PORT = 5647;
+const app = express();
+const server = app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
+
+// Static files
+app.use(express.static("public"));
+
+// Socket setup
+const io = socket(server);
+
+io.on("connection", function (socket) {
+    console.log("Made socket connection");
+
+    socket.on("message-packet", (arg) => {
+        console.log("Message recieved: " + arg)
+        socket.emit("message-response", arg);
+    });
+});
